19873aa45ef73eb1ca287a4275e493764006aa15
comaiivilvert <comaiivilvert@gmail.com>
2025-04-21 20:32:53 -0300

    Implementada versão com arquivos. Classe Robo.

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ExibirTamanhoDeMarte();
+
+            Console.WriteLine("Digite a posiçao inicial da espaçonave (X - Y - LETRA): ");
+            string posicaoXYZ = Console.ReadLine();
+            string[] posicaoInicial = posicaoXYZ.Split(' ');
+
+            Robo.iniX = int.Parse(posicaoInicial[0].ToString());
+            Robo.iniY = int.Parse(posicaoInicial[1].ToString());
+            Robo.sentidoInicial = char.Parse(posicaoInicial[2]);
+
+            //comando executado para a espaçonave percorrer o planeta e informar a coordenada que parou.
+            char[] instrucao = Comando();
+            Robo.Explorar(instrucao);
+            Robo.ExibirCoordenadas();
+
+        }
+
+        public static char[] Comando()
+        {
+            Console.WriteLine("Digite a instrução de vôo (MMEMMMDM): ");
+            string comando = Console.ReadLine();
+            char[] instrucao = comando.ToCharArray();
+            return instrucao;
+        }
+
+        public static void ExibirTamanhoDeMarte()
+        {
+            Console.WriteLine("Digite o tamanho do espaço a ser buscado em Marte (ex: 9 9)");
+            string tamanhoMarte = Console.ReadLine();
+            string[] posicaoMarte = tamanhoMarte.Split(' ');
+
+            int finalX = int.Parse(posicaoMarte[0].ToString());
+            int finalY = int.Parse(posicaoMarte[1].ToString());
         }
     }
 }
---
+++ RoboTupiniquim.ConsoleApp/Robo.cs
@@ -0,0 +1,86 @@
+﻿
+
+namespace RoboTupiniquim.ConsoleApp
+{
+    public static class Robo
+    {
+        public static int iniX;
+        public static int iniY;
+        public static char sentidoInicial;
+
+        public static void Explorar(char[] instrucao)
+        {
+            for (int i = 0; i < instrucao.Length; i++)
+            {
+                if (instrucao[i] == 'M')
+                {
+                    Mover();
+                }
+                else if (instrucao[i] == 'E')
+                {
+                    VirarEsquerda();
+                }
+                else
+                {
+                    VirarDireita();
+                }
+
+            }
+        }
+        public static void VirarEsquerda()
+        {
+            if (sentidoInicial == 'N')
+                sentidoInicial = 'O';
+
+            else if (sentidoInicial == 'S')
+                sentidoInicial = 'L';
+
+            else if (sentidoInicial == 'L')
+                sentidoInicial = 'N';
+
+            else if (sentidoInicial == 'O')
+                sentidoInicial = 'S';
+        }
+
+        public static void VirarDireita()
+        {
+            if (sentidoInicial == 'N')
+                sentidoInicial = 'L';
+
+            else if (sentidoInicial == 'S')
+                sentidoInicial = 'O';
+
+            else if (sentidoInicial == 'L')
+                sentidoInicial = 'S';
+
+            else if (sentidoInicial == 'O')
+                sentidoInicial = 'N';
+        }
+
+        internal static void Mover()
+        {
+            if (sentidoInicial == 'O')
+            {
+                iniX--;
+            }
+            else if (sentidoInicial == 'N')
+            {
+                iniY++;
+            }
+            else if (sentidoInicial == 'S')
+            {
+                iniY--;
+            }
+            else
+            {
+                iniX++;
+            }
+        }
+
+        public static void ExibirCoordenadas()
+        {
+            Console.WriteLine($"Você está na posição:{iniX} {iniY} {sentidoInicial}");
+            Console.ReadLine();
+        }
+    }
+}
